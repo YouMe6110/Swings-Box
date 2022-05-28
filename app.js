cfd5670eaf8b7381a8c68ed83b604c65366af8f4
YouMe6110 <ume0116@naver.com>
2022-05-28 22:14:52 +0900

    DAY 03

---
+++ app.js
@@ -0,0 +1,23 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2D');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize;
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientHeight;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth = this.pixelRatio;
+        this.canvas.height = this.stageHeight = this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    }
+}
