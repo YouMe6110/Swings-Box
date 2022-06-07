30058f46fb19c0eb37abb05bf0981a50cc5bbc7b
YouMe6110 <ume0116@naver.com>
2022-06-07 23:18:46 +0900

    DAY 13

--- app.js
+++ app.js
@@ -42,17 +42,27 @@ class App {
         this.ctx.shadowColor = 'rgba(0, 0, 0, 0.1';
 
         this.ctx.lineWidth = 2;
+
+        for (let i = 0; i < this.items.length; i++) {
+            this.items[i].resize(this.stageWidth, this.stageHeight);
+        }
     }
 
     animate() {
         Window.requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        for (let i = 0; i < this.items.length; i++) {
+            this.items[i].animate(this.ctx);
+        }
     }
 
     onDown(e) {
         this.mousePos.x = e.clientX;
         this.mousePos.y = e.clientY;
+
+        
     }
 
     onMove(e) {
